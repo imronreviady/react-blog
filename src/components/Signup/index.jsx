9b950a7716370540d9fd40be4cf79fa815012d0e
IMRON REVIADY <imronreviady2@gmail.com>
2018-08-21 03:49:48 +0700

    Customize error message on form signup

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,36 +1,99 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { validateAll } from 'indicative';
 
-const Signup = () => {
-	return (
-		<div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: 'url(assets/img/bg-girl.jpg)'}}>
-  			<div className="card card-shadowed p-50 w-400 mb-0" style={{maxWidth: '100%'}}>
-    			<h5 className="text-uppercase text-center">Register</h5>
-    			<br />
-    			<br />
-    			<form className="form-type-material">
-      				<div className="form-group">
-        				<input type="text" className="form-control" placeholder="Username" />
-      				</div>
-      				<div className="form-group">
-        				<input type="text" className="form-control" placeholder="Email address" />
-      				</div>
-      				<div className="form-group">
-        				<input type="password" className="form-control" placeholder="Password" />
-      				</div>
-      				<div className="form-group">
-        				<input type="password" className="form-control" placeholder="Password (confirm)" />
-      				</div>
-      				<br />
-      				<button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
-    			</form>
-    			<hr className="w-30" />
-    			<p className="text-center text-muted fs-13 mt-20">Already have an account?
-      				<Link to="/login">Sign in</Link>
-    			</p>
-  			</div>
-		</div>
-	);
-};
+class Signup extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			name: '',
+			email: '',
+			password: '',
+			password_confirmation: '',
+			errors: {}
+		};
+	}
+
+	handleInputChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		//validate user data
+		const data = this.state;
+		const rules = {
+			name: 'required|string',
+			email: 'required|email',
+			password: 'required|string|min:6|confirmed'
+		};
+
+		const messages = {
+			required: 'This {{ field }} is required.',
+			'email.email': 'The email is invalid.',
+			'password.confirmed': 'The password confirmation does not match.'
+		};
+
+		validateAll(data, rules, messages)
+			.then(() => {
+
+			})
+			.catch(errors => {
+				const formattedErrors = {};
+				errors.forEach(error => formattedErrors[error.field] = error.message);
+				this.setState({
+					errors: formattedErrors
+				});
+			});
+	}
+
+	render() {
+		return (
+			<div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: 'url(assets/img/bg-girl.jpg)'}}>
+	  			<div className="card card-shadowed p-50 w-400 mb-0" style={{maxWidth: '100%'}}>
+	    			<h5 className="text-uppercase text-center">Register</h5>
+	    			<br />
+	    			<br />
+	    			<form className="form-type-material" onSubmit={this.handleSubmit}>
+	      				<div className="form-group">
+	        				<input type="text" name="name" onChange={this.handleInputChange} className="form-control" placeholder="Username" />
+	        				{
+	        					this.state.errors['name'] &&
+	        					<small className="text-danger">{this.state.errors['name']}</small>
+	        				}
+	      				</div>
+	      				<div className="form-group">
+	        				<input type="text" name="email" onChange={this.handleInputChange} className="form-control" placeholder="Email address" />
+	        				{
+	        					this.state.errors['email'] &&
+	        					<small className="text-danger">{this.state.errors['email']}</small>
+	        				}
+	      				</div>
+	      				<div className="form-group">
+	        				<input type="password" name="password" onChange={this.handleInputChange} className="form-control" placeholder="Password" />
+	        				{
+	        					this.state.errors['password'] &&
+	        					<small className="text-danger">{this.state.errors['password']}</small>
+	        				}
+	      				</div>
+	      				<div className="form-group">
+	        				<input type="password" name="password_confirmation" onChange={this.handleInputChange} className="form-control" placeholder="Password (confirm)" />
+	      				</div>
+	      				<br />
+	      				<button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
+	    			</form>
+	    			<hr className="w-30" />
+	    			<p className="text-center text-muted fs-13 mt-20">Already have an account?
+	      				<Link to="/login">Sign in</Link>
+	    			</p>
+	  			</div>
+			</div>
+		);
+	}
+}
 
 export default Signup;
